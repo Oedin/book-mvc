36eaa7813fbca4bccbb5f949cb3b752518f1815f
edin <osmedin@gmail.com>
2017-06-28 18:22:15 -0500

    Routes

--- src/main/java/net/edinosmanovic/bookmvc/Hellocontroller.java
+++ src/main/java/net/edinosmanovic/bookmvc/Hellocontroller.java
@@ -1,8 +1,12 @@
 package net.edinosmanovic.bookmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by eo on 6/28/17.
@@ -13,12 +17,50 @@ public class Hellocontroller {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index(){
-        return "Welcome to our bookstore";
+    public String index(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+        if (name == null){
+            name = "Guest";
+        }
+
+        return name + ", welcome to our bookstore ";
     }
-    @RequestMapping(value = "bye")
+
+    @RequestMapping(value = "hello")
     @ResponseBody
+    public String helloForm(){
+        String html = "<form method = 'post'>"+
+                "<input type = 'text' name = 'name' />"+
+                "<input type ='submit' value = 'Greet me!'/>"+
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+
+        if (name == null){
+            name = "Guest";
+        }
+        return name + ", welcome to our bookstore";
+
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrl(@PathVariable String name){
+
+        return name +", welcome to our bookstore";
+    }
+
+
+    @RequestMapping(value = "bye")
     public String goodBye(){
-        return "Good Bye";
+        return "redirect:/";
     }
 }
